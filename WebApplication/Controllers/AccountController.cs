51885ac08b70b76a764c80066f766d71785bb16f
Nicasio <nicasiomontes97@gmail.com>
2019-03-23 22:43:46 -0300

    Code cleaning

--- WebApplication/Controllers/AccountController.cs
+++ WebApplication/Controllers/AccountController.cs
@@ -18,41 +18,6 @@ namespace WebApplication.Controllers
 
         public ActionResult LogInCheck(string userName, string password)
         {
-            #region ALTERNATIVA 1
-            //Domain.User user = new Domain.User();
-            //string actionName = null;
-            //string controllerName = null;
-            //object @object = null;
-
-            //if (!(String.IsNullOrWhiteSpace(userName) || String.IsNullOrWhiteSpace(password)))
-            //{
-            //    user = sql.LoadUser(userName, password);
-
-            //    if (!(user.UserName == null || user.Password == null))
-            //    {
-            //        actionName = "UserIndex";
-            //        controllerName = "User";
-            //        @object = new { userID = user.UserID };
-            //    }
-            //    else
-            //    {
-            //        actionName = "LogIn";
-            //        controllerName = "Account";
-            //        @object = new { message = "Incorrect username or password" };
-            //    }
-            //}
-            //else
-            //{
-            //    actionName = "LogIn";
-            //    controllerName = "Account";
-            //    @object = new { message = "Please, don't leave empty boxes" };
-            //}
-
-            //return RedirectToAction(actionName, controllerName, @object);
-            #endregion
-
-            #region ALTERNATIVA 3
-             
             string actionName = null;
             string controllerName = null;
             object @object = null;
@@ -88,6 +53,5 @@ namespace WebApplication.Controllers
         {
             return View();
         }
-        #endregion
     }
 }
--- WebApplication/Controllers/UserController.cs
+++ WebApplication/Controllers/UserController.cs
@@ -15,29 +15,6 @@ namespace WebApplication.Controllers
         // GET: User
         public ActionResult UserIndex(int userID)
         {
-            #region ALTERNATIVA 1
-            //Domain.User user = sql.LoadUser(userID);
-            //List<Domain.Movie> lm = sql.LoadMovies();
-            //ModelUser muser = new ModelUser();
-            //muser = muser.AsignDomainUser(user);
-            //return View(muser);
-            #endregion
-
-            #region ALTERNATIVA 2 (Error)
-            //Domain.User user = sql.LoadUser(userID);
-            //List<Domain.Movie> lm = sql.LoadMovies();
-
-            ////muser = muser.AsignDomainUser(user);
-            //object @object = new { User = user, MovieList = lm };
-            //return View(@object);
-            #endregion
-
-            #region ALTERNATIVA 3
-            //DomainModel dm = new DomainModel();
-            //dm.User = sql.LoadUser(userID);
-            //return View(dm.User);
-            #endregion
-
             User user = sql.LoadUser(userID);
             return View(user);
         }
